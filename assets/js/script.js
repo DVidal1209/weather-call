2ae43ef3ec7557b2e3d9e0d62ae261c3bcec8e98
Daniel Vidal <daniel.vidal17@yahoo.com>
2022-09-26 16:09:08 -0400

    Implementation of current weather conditions

---
+++ assets/js/script.js
@@ -0,0 +1,73 @@
+var searchBtn=document.querySelector("#search");
+var city=document.querySelector("#city");
+var curr=document.querySelector("#now");
+
+searchBtn.addEventListener("click", function(e){
+    e.preventDefault();
+    fetch("http://dataservice.accuweather.com/locations/v1/cities/search?apikey=FCNwacorZvGxY5Km0lgeueh3CHUE8QzH&q=" +city.value)
+        .then(function(res){
+            return res.json()
+        })
+        .then(function(data){
+            console.log("Data: ")
+            console.log(data)
+            fetch("http://dataservice.accuweather.com/currentconditions/v1/"+ data[0].Key + "?apikey=FCNwacorZvGxY5Km0lgeueh3CHUE8QzH&language=en&details=true")
+                .then(function(r){
+                    return r.json()
+                })
+                .then(function(d){
+                    console.log("Current:");
+                    console.log(d);
+                    var bigCard = document.createElement("div");
+                    bigCard.setAttribute("class", "card");
+                    bigCard.classList.add("col-12");
+                    bigCard.setAttribute("id", "current");
+                    var cardBody = document.createElement("div");
+                    cardBody.classList.add("card-body");
+                    var h5 = document.createElement("h5");
+                    var now = moment().format("DD/MM/YYYY")
+                    h5.innerHTML=data[0].Country.EnglishName + " (" + now + ")";
+                    var img = document.createElement("img");
+                    img.setAttribute("src", "./assets/images/"+d[0].WeatherIcon+".png");
+                    h5.appendChild(img);
+                    var temp = document.createElement("p");
+                    temp.innerHTML= "Temperature: " + d[0].Temperature.Imperial.Value + d[0].Temperature.Imperial.Unit;
+                    var wind = document.createElement("p");
+                    wind.innerHTML="Wind Speed: " + d[0].Wind.Speed.Imperial.Value + d[0].Wind.Speed.Imperial.Unit;
+                    var humidity = document.createElement("p");
+                    humidity.innerHTML = "Humidity: " + d[0].RelativeHumidity + "%";
+                    var uv = document.createElement("p");
+                    uv.innerHTML ="UV Index: ";
+                    var uvNum = document.createElement("span");
+                    uvNum.innerHTML =  d[0].UVIndex;
+
+                    //Color Coding of UV Index
+                    if (d[0].UVIndex<=2){
+                        uvNum.style.backgroundColor="green"
+                    } else if(d[0].UVIndex>=3 && d[0].UVIndex<=5){
+                        uvNum.style.backgroundColor="yellow"
+                    } if(d[0].UVIndex>=6 && d[0].UVIndex<=7){
+                        uvNum.style.backgroundColor="orange"
+                    } if(d[0].UVIndex>=8 && d[0].UVIndex<=10){
+                        uvNum.style.backgroundColor="red"
+                    } if(d[0].UVIndex>=11){
+                        uvNum.style.backgroundColor="purple"
+                    }
+                    uv.appendChild(uvNum);
+                    bigCard.appendChild(h5);
+                    bigCard.appendChild(temp);
+                    bigCard.appendChild(wind);
+                    bigCard.appendChild(humidity);
+                    bigCard.appendChild(uv);
+                    curr.appendChild(bigCard);
+                    fetch("http://dataservice.accuweather.com/forecasts/v1/daily/5day/" + data[0].Key+ "?apikey=FCNwacorZvGxY5Km0lgeueh3CHUE8QzH&details=true")
+                        .then(function(response){
+                            return response.json()
+                        })
+                        .then(function(dat){
+                            console.log("New Data:");
+                            console.log(dat);
+                        })
+                })
+        })
+})
